2b21178971472a3e6051fd597f2594449b28c3f0
prajwal <gajanangouda742006@gmail.com>
2026-02-23 18:57:00 +0530

    hiii

--- logic.js
+++ logic.js
@@ -1,1 +1,18 @@
-//logic
+function login() {
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+    let message = document.getElementById("message");
+
+    if (username === "" || password === "") {
+        message.style.color = "red";
+        message.innerHTML = "Please fill all fields";
+    }
+    else if (username === "admin" && password === "1234") {
+        message.style.color = "green";
+        message.innerHTML = "Login Successful";
+    }
+    else {
+        message.style.color = "red";
+        message.innerHTML = "Invalid Username or Password";
+    }
+}
